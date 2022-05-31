c3b37839696a5873828222438f6f7b1d2350e626
AndrewMLion <andrewmlion@gmail.com>
2022-05-31 12:48:04 +0300

    +

---
+++ js/test.js
@@ -0,0 +1,12 @@
+let base = prompt("Давай число");
+base = Number(base);
+
+console.log(base);
+
+let power = prompt("Давай степень");
+power = Number(power);
+console.log(power);
+
+const result = base ** power;
+
+console.log(result);
