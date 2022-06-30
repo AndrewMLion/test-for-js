08adbfcfade3a910e4a7a5b52b8b9ba955604bd7
AndrewMLion <andrewmlion@gmail.com>
2022-06-30 09:23:39 +0300

    example 19

--- js/autocheck.js
+++ js/autocheck.js
@@ -1,3 +1,4 @@
+//  Example JSV2 Block 3 19
 const products = [
     { name: 'Radar', price: 1300, quantity: 4 },
     { name: 'Scanner', price: 2700, quantity: 3 },
@@ -6,41 +7,12 @@ const products = [
 ];
 
 function getAllPropValues(propName) {
+    // Change code below this line
     const result = [];
-
-    for (const argument of products) {
-        // console.log(![propName] === argument);
-        // if ([propName] !== argument) {
-
-        // }
-        console.log(argument);
+    for (const product of products) {
+        if (product.hasOwnProperty([propName])) {
+            result.push(product[propName]);
+        }
     }
-
-    // return result;
+    return result;
 }
-// console.log(result);
-
-getAllPropValues('name');
-getAllPropValues('quantity');
-getAllPropValues('price');
-getAllPropValues('category');
-
-//  В браузере работает, автопроверка не принимает (
-
-// const products = [
-//     { name: 'Radar', price: 1300, quantity: 4 },
-//     { name: 'Scanner', price: 2700, quantity: 3 },
-//     { name: 'Droid', price: 400, quantity: 7 },
-//     { name: 'Grip', price: 1200, quantity: 9 },
-// ];
-
-// function getAllPropValues(propName) {
-//     // Change code below this line
-//     const result = [];
-//     for (const product of products) {
-//         if (product.hasOwnProperty([propName])) {
-//             result.push(product[propName]);
-//         }
-//         // Change code above this line
-//     }
-// }
