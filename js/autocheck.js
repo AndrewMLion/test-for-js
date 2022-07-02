51b88e043e62db3ba5467e8df2b1b0a4a2568aac
AndrewMLion <andrewmlion@gmail.com>
2022-07-02 11:56:36 +0300

    решение 20 задачи

--- js/autocheck.js
+++ js/autocheck.js
@@ -1,18 +1,56 @@
-//  Example JSV2 Block 3 19
-const products = [
-    { name: 'Radar', price: 1300, quantity: 4 },
-    { name: 'Scanner', price: 2700, quantity: 3 },
-    { name: 'Droid', price: 400, quantity: 7 },
-    { name: 'Grip', price: 1200, quantity: 9 },
-];
+// **** Example JSV2 Block 3 19
 
-function getAllPropValues(propName) {
-    // Change code below this line
-    const result = [];
-    for (const product of products) {
-        if (product.hasOwnProperty([propName])) {
-            result.push(product[propName]);
-        }
-    }
-    return result;
-}
+// const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//     const result = [];
+//     for (const product of products) {
+//         if (product.hasOwnProperty([propName])) {
+//             result.push(product[propName]);
+//         }
+//     }
+//     return result;
+// }
+
+// getAllPropValues('name');
+// getAllPropValues('quantity');
+// getAllPropValues('price');
+// getAllPropValues('category');
+
+// console.log(getAllPropValues('name'));
+
+//  ***** Example JSV2 Block 3 20
+
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   let result = 0;
+
+//   for (const product of products) {
+//     const { name, price, quantity } = product;
+
+//     if (name === productName) {
+//       return (result = price * quantity);
+//     }
+//   }
+//   return result;
+// }
+
+// calculateTotalPrice('Blaster');
+// calculateTotalPrice('Radar');
+// calculateTotalPrice('Droid');
+// calculateTotalPrice('Grip');
+// calculateTotalPrice('Scanner');
+// console.log(calculateTotalPrice(productName));
+
+//  ***** Example JSV2 Block 3 21
