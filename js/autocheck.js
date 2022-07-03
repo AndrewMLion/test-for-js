0ce34b1f32e18fc84a65fc495c7db36710ce3f8c
AndrewMLion <andrewmlion@gmail.com>
2022-07-03 13:50:15 +0300

    Решение 29 задачи

--- js/autocheck.js
+++ js/autocheck.js
@@ -189,3 +189,17 @@
 // console.log('worstScore: ', worstScore);
 
 //  ***** Example JSV2 Block 3 28
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [
+//   ...firstGroupScores,
+//   ...secondGroupScores,
+//   ...thirdGroupScores,
+// ];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+
+//  ***** Example JSV2 Block 3 29
